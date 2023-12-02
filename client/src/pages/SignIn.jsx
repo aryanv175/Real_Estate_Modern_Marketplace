3044bb4e5813eda15ca6e038c3abb661b06c32df
aryanv175 <aryanv1753@gmail.com>
2023-12-02 18:32:29 -0500

    src/pages completed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -46,41 +46,39 @@ export default function SignIn() {
   };
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <div className='p-6 bg-white rounded-md shadow-md max-w-md w-full'>
-        <h1 className='text-3xl text-center font-semibold mb-6'>Sign In</h1>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-          <input
-            type='email'
-            placeholder='Email'
-            className='border p-3 rounded-lg focus:outline-none focus:border-blue-500'
-            id='email'
-            onChange={handleChange}
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            className='border p-3 rounded-lg focus:outline-none focus:border-blue-500'
-            id='password'
-            onChange={handleChange}
-          />
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-          <button
-            disabled={loading}
-            className='bg-blue-500 text-white p-3 rounded-lg uppercase hover:bg-blue-700 disabled:opacity-80 cursor-pointer'
-          >
-            {loading ? 'Loading...' : 'Sign In'}
-          </button>
-          <OAuth/>
-        </form>
-        <div className='flex items-center justify-center mt-5'>
-          <p>Don't have an account?</p>
-          <Link to={'/sign-up'} className='text-blue-700 ml-2'>
-            Sign up
-          </Link>
-        </div>
-        {error && <p className='text-red-500 mt-5'>{error}</p>}
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+        <OAuth/>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
