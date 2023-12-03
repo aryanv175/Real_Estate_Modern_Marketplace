2e402441101fba81facd8edc38a5e1151d0a7d9f
yash1506 <yashsinhal15@gmail.com>
2023-12-03 18:06:40 -0500

    change home route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header() {
             </button> 
         </form>
         <ul className='flex gap-4'>
-            <Link to='/home'>
+            <Link to='/'>
                 <li className='hidden sm:inline text-blue-700 hover:underline'>Home</li>
             </Link>
             <Link to='/about'>
